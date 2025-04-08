caf63614048dfcd77264c99c4c366f82d960e4c7
ALOK KUMAR SINGH <alok2026@mpgi.edu.in>
2025-04-09 01:30:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import styles from "./App.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { useState } from "react";
+
+
+function App() {
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+
+  return (
+    <center><h1>Calculator</h1>
+      <div className={styles.calculator}>
+        <Display displayValue={calVal}></Display>
+        <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+      </div>
+    </center>
+  );
+}
+
+export default App;
